63cad715eb85b4bf4929957866b8f3329ebf8f74
Hanyu.King <wanghanyu8@jd.com>
2019-04-01 17:38:16 +0800

    OOM Case

--- src/main/java/oter/OperatorTest.java
+++ src/main/java/oter/OperatorTest.java
@@ -2,6 +2,11 @@ package oter;
 
 public class OperatorTest {
     public static void main(String[] args) {
+        /**
+         * 001
+         * 010
+         * 100
+         */
         System.out.println(1 | 2 | 8);
     }
 }
